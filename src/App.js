011e8b66895cd82955fbd2765ab4c2f52fb2448d
eles linux <eleswastaken@gmail.com>
2022-04-20 18:30:52 +0600

    Create countdown

--- src/App.js
+++ src/App.js
@@ -1,9 +1,59 @@
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  return (
-    <div className="App">
+	
+	// let sessionLength = 1500_000 // ms
+	let sessionLength = 10_000 // ms
 
-    </div>
+	const [time, setTime] = useState(sessionLength);
+	const [isOn ,setIsOn] = useState(false);
+
+	let startTime = 0;
+	let endTime = 0
+
+	function start() {
+		if (!isOn) {
+
+			startTime = Date.now();
+			endTime = startTime + sessionLength;
+
+			setIsOn(true)
+
+			interval()
+		}
+	}
+
+	function interval() {
+		
+		let now = Date.now();
+		
+		if (now <= endTime) {
+			
+			let leftTime = endTime - now;
+
+			setTime(leftTime + 700) // 700 is a number for the user
+			setTimeout(interval, 500)
+
+		} else {
+
+			console.log('session is over')
+
+			setIsOn(false)
+
+			// save the session
+
+		}
+	}
+
+	return (
+		<div className="App">
+
+			<p id="dom-timer">{ `${("0" + Math.floor(time/1000/60)).slice(-2)}:${("0" + Math.floor(time/1000%60)).slice(-2)}` }
+			</p>
+
+			<button onClick={ start }>Start</button>
+
+		</div>
   );
 }
 
