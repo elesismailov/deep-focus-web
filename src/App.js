918dd989c5c7b6951b5f83ee3628234bf6f70355
eles linux <eleswastaken@gmail.com>
2022-04-22 08:52:25 +0600

    Prepare work/break sessions

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Timer from './Components/Timer/Timer';
 
 function App() {
 
+	// 1 - work, 2 - break; this will decide whether to record it or not
+	const [mode, setMode] = useState(1);
+	// [workLength, breakLength, ...]
+	const [sessions, setSessions] = useState([10000, 5000, 10000]);
+	// the number to access the current session
+	const [sessionNumber, setSessionNumber] = useState(0);
+
+	const nextSession = () => sessions.length-1 === sessionNumber ? setSessionNumber(0) : setSessionNumber(sessionNumber+1);
+	const switchMode = () => setMode(mode === 1 ? 2 : 1)
+
 	return (
 		<div className="App">
-			<Timer />
+			<p>Mode: { mode }</p>
+			<p>Session: { sessionNumber }</p>
+			<Timer mode={ mode } setMode={ setMode } />
 		</div>
 	);
 }
