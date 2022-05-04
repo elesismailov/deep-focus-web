8b2392589df1ef12c3ec15a83898a8ccf13670e3
eles linux <eleswastaken@gmail.com>
2022-05-04 18:56:57 +0600

    Add audio

--- src/Components/Audio.js
+++ src/Components/Audio.js
@@ -1,18 +1,27 @@
+import { useRef } from 'react';
 
 import TimerState from './TimerState';
 
+import finish1 from './audio/finish-1.wav';
+
 
 function Audio() {
+	const audioEl = useRef(null);
 
 	function play(mode) {
 
-		console.log('Audio notification')
-		
+		if (mode === 1) { // audio for the work session
+			audioEl.current.play()
+		} else if ( mode === 0) { // audio for the brake session
+			audioEl.current.play()
+		}
+
 	}
 
 	return(
 		<>	
 			<TimerState play={play}/>
+			<audio ref={ audioEl } src={ finish1 }></audio>
 		</>
 	);
 
