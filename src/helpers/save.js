7b430c97db38eaa4a43c3c363ca22417ae741b4a
eles linux <eleswastaken@gmail.com>
2022-05-06 15:32:22 +0600

    Create saveSession function

--- src/helpers/save.js
+++ src/helpers/save.js
@@ -1,14 +1,22 @@
 
 
-async function saveSession({start, end}) {
+async function saveSession(startTime, endTime) {
 
-	console.log('here happens fetch')
+	try {
 
-	const res = await fetch('http://127.0.0.1:5000/entries', {
-		method: 'POST'
-	});
+		const res = await fetch('http://127.0.0.1:5000/entries', {
+			method: 'POST',
+			headers: {
+				"Authorization": "Bearer " + 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6InRlc3QtdXNlci1uYW1lIiwiZW1haWwiOiJ0ZXN0dXNlckBnbWFpbC5jb20ifQ.tM5LFeshVJS7rPnz6KS8hngIOuImC5dM8HyGrsxQzHY',
+				// "Access-Control-Allow-Origin": "*",
+			}	,
+			body: JSON.stringify({ startTime, endTime})
+		});
 
-	console.log(res)
+	} catch (err) {
+		console.log(err)
+		console.log('save locally instead')	
+	}
 
 }
 
