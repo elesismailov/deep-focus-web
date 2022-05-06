64f224eaef6f03bb38d4806cee8ad3204365c6a5
eles linux <eleswastaken@gmail.com>
2022-05-06 17:34:36 +0600

    Add a way to set token

--- src/AppState.js
+++ src/AppState.js
@@ -1,4 +1,6 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+
+import { setSaveToken } from './helpers/save';
 
 import Audio from './Components/Audio';
 import LogIn from './Components/LogIn';
@@ -12,6 +14,13 @@ function AppState() {
 	const [loggedIn, setLoggedIn] = useState(null);
 	const [token, setToken] = useState(null);
 
+	// this is a way to pass the token not through components but closures
+	useEffect(() => {setSaveToken(token) }, [token])
+
+	// delete this later
+	useEffect(() => {
+		setToken('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6InRlc3QtdXNlci1uYW1lIiwiZW1haWwiOiJ0ZXN0dXNlckBnbWFpbC5jb20ifQ.tM5LFeshVJS7rPnz6KS8hngIOuImC5dM8HyGrsxQzHY')
+	}, [])
 
 	return(
 		<>
--- src/helpers/save.js
+++ src/helpers/save.js
@@ -1,12 +1,19 @@
 
+let token = '';
+
+function setSaveToken(t) {
+	token = t
+}
 
 async function saveSession(startTime, endTime) {
+
+	// const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6InRlc3QtdXNlci1uYW1lIiwiZW1haWwiOiJ0ZXN0dXNlckBnbWFpbC5jb20ifQ.tM5LFeshVJS7rPnz6KS8hngIOuImC5dM8HyGrsxQzHY';
 
 	try {
 		const res = await fetch('http://localhost:5000/entries', {
 			method: 'POST',
 			headers: {
-				"Authorization": "Bearer " + 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6InRlc3QtdXNlci1uYW1lIiwiZW1haWwiOiJ0ZXN0dXNlckBnbWFpbC5jb20ifQ.tM5LFeshVJS7rPnz6KS8hngIOuImC5dM8HyGrsxQzHY',
+				"Authorization": "Bearer " + token,
 				// "Access-Control-Allow-Origin": "*",
 				"Content-Type": 'application/json',
 			}	,
@@ -21,4 +28,4 @@ async function saveSession(startTime, endTime) {
 }
 
 
-export { saveSession }
+export { saveSession, setSaveToken }
