802e03a7bd630cc7011beb950adb94011ee6b7cf
eles linux <eleswastaken@gmail.com>
2022-05-06 16:31:43 +0600

    Refactor

--- src/Components/TimerState.js
+++ src/Components/TimerState.js
@@ -7,7 +7,7 @@ function TimerState({ play }) {
 	const [mode, setMode] = useState(1);
 
 	// [workLength, breakLength, ...] ALWAYS the first session is work
-	const [sessions, setSessions] = useState([10000, 5000, 10000, 3000]);
+	const [sessions, setSessions] = useState([3000, 2000, 3000, 2000]);
 	// const [sessions, setSessions] = useState([2000_000, 600_000, 2000_000]);
 
 	// the number to access the current session
--- src/helpers/save.js
+++ src/helpers/save.js
@@ -3,14 +3,14 @@
 async function saveSession(startTime, endTime) {
 
 	try {
-
-		const res = await fetch('http://127.0.0.1:5000/entries', {
+		const res = await fetch('http://localhost:5000/entries', {
 			method: 'POST',
 			headers: {
 				"Authorization": "Bearer " + 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6InRlc3QtdXNlci1uYW1lIiwiZW1haWwiOiJ0ZXN0dXNlckBnbWFpbC5jb20ifQ.tM5LFeshVJS7rPnz6KS8hngIOuImC5dM8HyGrsxQzHY',
 				// "Access-Control-Allow-Origin": "*",
+				"Content-Type": 'application/json',
 			}	,
-			body: JSON.stringify({ startTime, endTime})
+			body: JSON.stringify({ startTime, endTime })
 		});
 
 	} catch (err) {
