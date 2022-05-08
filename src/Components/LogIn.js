bb659dab73bbbe16dad93221fc970dc9f46ee7f7
eles linux <eleswastaken@gmail.com>
2022-05-08 12:10:48 +0600

    Create log in component

--- src/Components/LogIn.js
+++ src/Components/LogIn.js
@@ -1,10 +1,29 @@
 
+import { login } from '../helpers/login';
 
-function LogIn() {
+function LogIn({ setToken, setLoggedIn }) {
+
+	function cb(token) {
+		setToken(token)
+		setLoggedIn(true)
+	}
 
 	return(
 		<>
-
+			<form onSubmit={ (e) => login(e, cb) }>
+				<label>
+					<p>Email:</p>
+					<input type="text" name="email" required />
+				</label>
+				<label>
+					<p>Password:</p>
+					<input type="password" name="password" required />
+				</label>
+				<button>Submit</button>
+			</form>
 		</>
 	);
 }
+
+
+export default LogIn;
