b17a0e8c7aa40a028b063384115f2c6bd61dd964
eles linux <eleswastaken@gmail.com>
2022-05-10 17:53:38 +0600

    Refactor

--- src/helpers/save.js
+++ src/helpers/save.js
@@ -30,7 +30,6 @@ async function restoreAppState(cb) {
 
 async function saveSession(startTime, endTime) {
 
-	// const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6InRlc3QtdXNlci1uYW1lIiwiZW1haWwiOiJ0ZXN0dXNlckBnbWFpbC5jb20ifQ.tM5LFeshVJS7rPnz6KS8hngIOuImC5dM8HyGrsxQzHY';
 
 	try {
 		const res = await fetch('http://localhost:5000/entries', {
